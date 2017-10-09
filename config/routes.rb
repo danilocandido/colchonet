b48872c31c10fdc3ccbaf9c154c4dbcab5730260
Danilo Cândido <danilod07@gmail.com>
2017-10-09 00:22:39 -0300

    email confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :rooms
   resources :users  
+  resource :confirmation, only: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170924234759) do
+ActiveRecord::Schema.define(version: 20171009025125) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20170924234759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.datetime "confirmed_at"
+    t.string "confirmation_token"
   end
 
 end
